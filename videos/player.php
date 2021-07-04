e37f48c18af4df3533f368080136c090b4fcd050
Joseph The Animator <73652344+josephcrosmanplays532@users.noreply.github.com>
2021-07-04 09:35:47 -0400

    Update player.php

--- videos/player.php
+++ videos/player.php
@@ -294,8 +294,8 @@ if (self !== top) {
                     <span class="icon-bar"></span>
                     <span class="icon-bar"></span>
                   </button>
-                  <a class="navbar-brand" href="https://ga.vyond.com/yourvideos" title="Vyond">
-                      <img alt="Vyond" src="https://josephcrosmanplays532.github.io/static/55910a7cd204c37c/go/img/vyond/vyond_logo_legacy.png">
+                  <a class="navbar-brand" href="./browse.php" title="PHPWrapper">
+                      <img alt="Vyond" src="/pages/img/list_logo.svg">
                   </a>
             </div>
             <div class="collapse navbar-collapse navbar-ex1-collapse">

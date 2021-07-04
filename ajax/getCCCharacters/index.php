1967f8c74f7833e1a19b1bfbffbdb2e5114f236f
Joseph The Animator <73652344+josephcrosmanplays532@users.noreply.github.com>
2021-07-04 17:16:19 -0400

    Update index.php

--- ajax/getCCCharacters/index.php
+++ ajax/getCCCharacters/index.php
@@ -1,3 +1,18 @@
 <?php
-	// IDK.
+	////
+	//// This PHP loads characters
+	////
+
+	///
+	/// Variables
+	///
+	$charId = $_POST["assetId"];
+
+	///
+	/// Read file
+	///
+	// The 0 lets the Video Studio know it worked.
+	echo "0";
+	// Read the file
+	readfile("../../_user-files/characters/character-xml-{$charId}.xml");
 ?>

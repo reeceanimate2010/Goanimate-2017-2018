90d6378b9747e59ba7373f13de90a6eaa2c2b6c6
Joseph The Animator <73652344+josephcrosmanplays532@users.noreply.github.com>
2021-07-07 15:40:20 -0400

    Update start.bat

--- start.bat
+++ start.bat
@@ -1,32 +1,118 @@
-:: Important stuff
+:: Starts up PHPWrapper
+:: Author: octanuary#6596
+:: License: GPLv3
+set PWRAP_VER=0.1.1
+title PHPWrapper v%PWRAP_VER%
+
+:: Initialize (stop command spam, clean screen, make variables work, set to UTF-8)
 @echo off && cls
-title PHPWrapper
+SETLOCAL ENABLEDELAYEDEXPANSION
+chcp 65001 >nul
 
-::::::::::::::::::::
-:: Initialization ::
-::::::::::::::::::::
+:: Move to base folder, and make sure it worked
+pushd "%~dp0"
+if not exist dependencies ( goto starting_error )
+if not exist notinstalled ( goto starting_error )
+goto no_start_error
+:starting_error
+echo It doesn't seem like this script is in the main PHPWrapper.
+goto end
+:no_start_error
 
-:: Terminate existing php apps
-TASKKILL /IM php.exe /F 2>nul
-cls
-
-::::::::::::::::::::::
-:: Start PHPWrapper ::
-::::::::::::::::::::::
-
-:: Check for installation
 if exist notinstalled (
-	echo PHPWrapper is not installed! Installing...
-	call npm install
-	ren "notinstalled" "installed"
-	cls
-	goto start
-) else (
-	goto start
+ren "notinstalled" "installed"
 )
 
-:: Run npm start
-:start
-echo PHPWrapper is now starting...
-echo Please navigate to http://localhost on your browser.
-npm start
+:: Option
+echo:
+echo Enter 1 to start with options
+echo Enter 2 to start without options
+echo Enter 0 to exit
+echo:
+:reask_a
+set /p OPTION= What would you like to do? 
+if !OPTION!==1 ( goto launch )
+if !OPTION!==2 ( goto launch_no_option )
+if !OPTION!==0 ( goto end )
+echo You have not chosen a valid option.
+goto reask_a
+
+
+:launch_no_option
+:: Launch ungoogled-chromium
+title PHPWrapper v!PWRAP_VER! [Starting...]
+echo:
+echo PHPWrapper v!PWRAP_VER! is starting...
+echo A project from Octanuary
+echo:
+echo Launching ungoogled-chromium...
+pushd dependencies\ungoogled-chromium
+start chromium.exe --user-data-dir=the_profile --app=http://localhost:6596 --allow-outdated-plugins
+popd
+
+:: Start PHPWrapper
+pushd phpwrapper
+echo:
+echo Starting PHPWrapper...
+echo:
+call ..\dependencies\php\php.exe -S 127.0.0.1:6596
+echo:
+popd
+echo PHP has been closed.
+goto end
+
+
+:: Launch ungoogled-chromium
+:launch
+title PHPWrapper v!PWRAP_VER! [Starting...]
+echo:
+echo PHPWrapper v!PWRAP_VER! is starting...
+echo A project from Octanuary
+echo:
+echo Launching ungoogled-chromium...
+pushd dependencies\ungoogled-chromium
+start chromium.exe --user-data-dir=the_profile --app=http://localhost:6596 --allow-outdated-plugins
+popd
+
+:: Launch PHPWrapper in separate window
+pushd dependencies
+start /MIN launch.bat
+popd
+
+:::::::::::::
+:: Options ::
+:::::::::::::
+
+title PHPWrapper v!PWRAP_VER!
+:cls
+cls
+echo:
+echo Enter 1 to relaunch ungoogled-chromium
+echo Enter cls to clear the screen
+echo Enter 0 to exit
+echo:
+:reask_b
+set /p OPTION= What would you like to do? 
+if "!OPTION!"=="1" goto reopen_chromium
+if /i "!OPTION!"=="cls" goto cls
+if "!OPTION!"=="0" goto end
+echo You have not chosen a valid option.
+goto reask_b
+
+
+:reopen_chromium
+:: Launch ungoogled-chromium
+echo:
+echo PHPWrapper v!PWRAP_VER! is starting...
+echo A project from Octanuary
+echo:
+echo Launching ungoogled-chromium...
+pushd dependencies\ungoogled-chromium
+start chromium.exe --user-data-dir=the_profile --allow-outdated-plugins
+popd
+echo Chromium has been opened.
+goto reask_b
+
+:end
+echo Closing script...
+pause & exit

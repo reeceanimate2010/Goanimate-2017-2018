2d1bffd0373007a4c4b470c159ff8e55a85da391
Joseph The Animator <73652344+josephcrosmanplays532@users.noreply.github.com>
2021-07-07 23:19:40 -0400

    Update oldvideobrowser.php

--- videos/oldvideobrowser.php
+++ videos/oldvideobrowser.php
@@ -17,8 +17,8 @@
 <meta name="google-site-verification" content="K_niiTfCVi72gwvxK00O4NjsVybMutMUnc-ZnN6HUuA">
 
 
-<link href="https://josephcrosmanplays532.github.io/GoAnimate-2016-Video-Player-Files/font.css" rel="stylesheet" type="text/css">
-<link href="https://josephcrosmanplays532.github.io/GoAnimate-2016-Video-Player-Files/common_combined.css" rel="stylesheet" type="text/css">
+<link href="https://josephcrosmanplays532.github.io/fonts/1/sailec.css" rel="stylesheet" type="text/css">
+<link href="https://josephcrosmanplays532.github.io/static/55910a7cd204c37c/go/css/common_combined.css" rel="stylesheet" type="text/css">
 	
 <link href="/pages/css/list-light.css" rel="stylesheet" type="text/css">
 	
@@ -108,59 +108,77 @@ height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
 <div class="page-container">
 
 
-<nav class="navbar site-nav" role="navigation">
-    <div class="container">
-        <div class="navbar-header">
-            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target=".navbar-ex1-collapse">
-                <span class="sr-only">Toggle navigation</span>
-                <span class="icon-bar"></span>
-                <span class="icon-bar"></span>
-                <span class="icon-bar"></span>
-              </button>
-            <a class="navbar-brand" href="/dashboard/videos" title="Vyond">
-                <img src="https://josephcrosmanplays532.github.io/GoAnimate-2016-Video-Player-Files/vyond_logo_legacy_goanimate.png" alt="Vyond">
-            </a>
-        </div>
-        <ul class="nav site-nav-alert-nav hidden-xs">
-            <li>
-                <a href="/notifications" title="Notifications"><span class="glyph-pro glyph-bell"></a>
-            </li>
-        </ul>
-        <div class="collapse navbar-collapse navbar-ex1-collapse">
-            <ul class="nav navbar-nav navbar-right">
-                <li class="dropdown">
-                    <a class="dropdown-toggle" href="#" data-toggle="dropdown">Your Account <span class="caret"></span></a>
-                    <ul class="dropdown-menu">
-                        <li><a href="/student">Dashboard</a></li>
-                        <li><a href="/dashboard/videos">Your Videos</a></li>
-                        <li class="divider"></li>
-                        <li><a href="/account">Account Settings</a></li>
-                        <li><a href="/profile/you">Your Profile</a></li>
-                        <li class="divider"></li>
-                        <li><a class="logout-link" href="/logoff">Logout</a></li>
-                    </ul>
-                </li><li class="dropdown">
-                    <a class="dropdown-toggle" href="#" data-toggle="dropdown">Explore <span class="caret"></span></a>
-                    <ul class="dropdown-menu">
-                        <li><a href="/students">Students</a></li>
-                        <li><a href="/teachers">Teachers</a></li>
-                        <li><a href="/videos">Videos</a></li>
-                        <li><a href="/public_faq">FAQ</a></li>
-                    </ul>
-                </li>
-                <li>
-				<a class="hidden-sm hidden-md hidden-lg" href="/videomaker">Make a Video</a>
-				<span class="site-nav-btn hidden-xs"><a class="btn btn-orange" href="/videomaker">Make a Video</a></span>
-                </li>
-            </ul>
-        </div>
+<div class="site-header">
+    <div class="navbar site-nav site-nav--legacy" role="navigation">
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target=".navbar-ex1-collapse">
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                  </button>
+                  <a class="navbar-brand" href="browse.php" title="PHPWrapper">
+                      <img alt="Vyond" src="/pages/img/list_logo.svg">
+                  </a>
+            </div>
+            <div class="collapse navbar-collapse navbar-ex1-collapse">
+                
+<ul class="nav navbar-nav navbar-right">
+<li class="dropdown">
+    <a class="dropdown-toggle" href="https://discord.gg/YYFenX5Fep" data-toggle="dropdown">Help<span class="dropdown-caret"></span></a>
+    <ul class="dropdown-menu dropdown-menu-help">
+        <li>
+            <a href="https://discord.gg/YYFenX5Fep">Help Center</a>
+	    <a data-remote="/ajax/watchMovie/JosephAnimateAwsomeDomainDemo" data-action="video-share">Joseph Animate Awesome Domain Demo</a>
+        </li>
+    </ul>
+</li>
+<script>
+$('.video-actions').toggle($('.video-actions').find('.btn').length > 0);
+</script>
+	<li>
+		<a class="btn btn-small" href="./player.php?video=<?php if (isset($_GET["video"])) { echo $_GET["video"]; } else { echo "null"; } ?>&videowidth=0">Watch your video in the old style</a>
+	</li>
+	<li>
+		<a class="btn btn-small" data-remote="/ajax/embedMovie/?video=<?php if (isset($_GET["video"])) { echo $_GET["video"]; } else { echo "null"; } ?>" data-action="video-share">Embed Your Video</a>
+	</li>
+                    <li>
+                        <a class="hidden-sm hidden-md hidden-lg" href="/create.php">Make a Video</a>
+                        <span class="site-nav-btn hidden-xs"><a class="btn btn-orange" href="/create.php">Make a Video</a></span>
+                    </li>
+<li class="dropdown">
+    <a class="dropdown-toggle" href="https://app.vyond.com/v2/profile" data-toggle="dropdown">
+        <span class="hidden-sm hidden-md hidden-lg">Your Account</span>
+        <div class="site-nav__profile-image">
+            <div class="badge-circle">JA</div>
+        </div><span class="dropdown-caret"></span>
+    </a>
+    <ul class="dropdown-menu dropdown-menu-user">
+        <li class="dropdown-user-profile">
+            <div class="dropdown-user-profile__display-name">
+                Joseph Animate 2021 (Owner)            </div>
+        </li>
+        <li class="divider"></li>
+        <li><a href="/profile/you.html">Profile Settings</a></li>
+        <li><a href="https://app.vyond.com/v2/users/list">Users</a></li>
+        <li><a href="https://app.vyond.com/v2/security">Security</a></li>
+        <li><a href="https://app.vyond.com/v2/subscription">Subscription</a></li>
+        <li class="divider"></li>
+        <li><a href="https://app.vyond.com/video/list" onClick="amplitudeTrackSwitchVideoMaker('Go to Vyond Studio')">Go to Vyond Studio</a></li>
+        <li class="divider"></li>
+        <li><a class="gtm-logout" href="https://ga.vyond.com/logoff">Log Out</a></li>
+    </ul>
+</li>
+      </ul>
+                
+            </div>
     </div>
-</nav>
+</div>
 
 <!-- END OF HEADER -->
 
 <div class="warning">
-    Vyond 2019 is in process</div>
+    PHPWrapper is in process</div>
 <script>
     (function(){
         // Log last upsell date.
@@ -365,84 +383,28 @@ var folderCounts = 0;
     </div>
 </div>
 
-
+<div id="video-share" class="modal" role="dialog" aria-hidden="true">
+  <div class="modal-dialog"><div class="modal-content"></div></div>
+</div>
 <!-- FOOTER -->
 
-<div class="site-footer">
+<footer class="site-footer">
     <div class="container">
-        <div class="site-footer-nav row">
-            <div class="col-sm-3">
-                <div class="site-footer-nav-col">
-                    <h4><span>About GoAnimate</span></h4>
-                    <ul class="list-unstyled">
-                        <li><a href="http://web.archive.org/web/20180307222818/https://goanimate.com/about">Who we are</a></li>
-                        <li><a href="http://web.archive.org/web/20180307222818/https://resources.goanimate.com/careers/">Careers</a></li>
-                        <li><a href="http://web.archive.org/web/20180307222818/https://goanimate.com/contactus">Contact Us</a></li>
-                        <li><a href="http://web.archive.org/web/20180307222818/https://blog.goanimate.com/">Blog</a></li>
-                        <li><a href="http://web.archive.org/web/20180307222818/https://press.goanimate.com/">Press</a></li>
-                    </ul>
-                </div>
-            </div>
-            <div class="col-sm-3">
-                <div class="site-footer-nav-col">
-                    <h4><span>GoAnimate Solutions</span></h4>
-                    <ul class="list-unstyled">
-                        <li><a href="http://web.archive.org/web/20180307222818/https://goanimate.com/business/videoplans?hook=footer_button.site">Plans and Pricing</a></li>
-                        <li><a href="http://web.archive.org/web/20180307222818/https://goanimate.com/business/videoplans/federal?hook=footer_button.site">Plans for Federal Agencies</a></li>
-                        <li><a href="http://web.archive.org/web/20180307222818/https://goanimate4schools.com/" target="_blank">GoAnimate for Schools</a></li>
-                    </ul>
-                </div>
-            </div>
-            <div class="col-sm-3">
-                <div class="site-footer-nav-col">
-                    <h4><span>Usage Guidelines</span></h4>
-                    <ul class="list-unstyled">
-                        <li><a href="http://web.archive.org/web/20180307222818/https://goanimate.com/termsofuse">Terms of Service</a></li>
-                        <li><a href="http://web.archive.org/web/20180307222818/https://goanimate.com/privacy">Privacy Policy</a></li>
-                        <li><a href="//web.archive.org/web/20180307222818/https://support.goanimate.com/hc/en-us/articles/202408574" target="_blank">Cancellation Policy</a></li>
-                    </ul>
-                </div>
-            </div>
-            <div class="col-sm-3">
-                <div class="site-footer-nav-col">
-                    <h4>Getting Help</h4>
-                    <ul class="list-unstyled">
-                        <li><a href="http://web.archive.org/web/20180307222818/https://resources.goanimate.com/">Resources</a></li>
-                        <li><a href="http://web.archive.org/web/20180307222818/https://support.goanimate.com/">Help Center</a></li>
-                    </ul>
-                </div>
-            </div>
-        </div>
-        <hr>
-        <div class="row">
-            <div class="col-sm-6">
-                <div class="site-footer-socials-container">
-                    Follow us on:
-                    <ul class="site-footer-socials">
-                        <li><a class="glyph-social glyph-facebook" href="http://web.archive.org/web/20180307222818/https://www.facebook.com/GoAnimateInc"><span class="sr-only">Facebook</span></a></li>
-                        <li><a class="glyph-social glyph-twitter" href="http://web.archive.org/web/20180307222818/https://twitter.com/GoAnimate"><span class="sr-only">Twitter</span></a></li>
-                        <li><a class="glyph-social glyph-linked-in" href="http://web.archive.org/web/20180307222818/https://www.linkedin.com/company/goanimate"><span class="sr-only">LinkedIn</span></a></li>
-                        <li><a class="glyph-social glyph-google-plus" href="http://web.archive.org/web/20180307222818/https://plus.google.com/+goanimate"><span class="sr-only">Google+</span></a></li>
-                        <li><a class="glyph-social glyph-youtube" href="http://web.archive.org/web/20180307222818/https://www.youtube.com/user/GoAnimate"><span class="sr-only">YouTube</span></a></li>
-                    </ul>
-                </div>
-            </div>
-            <div class="col-sm-6">
-                <div class="site-footer-copyright">
-                    <img src="http://web.archive.org/web/20180307222818im_/https://d3v4eglovri8yt.cloudfront.net/static/f789d2b2551c1fb6/go/img/footer/logo_amazon.png" alt="AWS Partner Network">
-                    &nbsp;&nbsp;&nbsp;
-                    <div class="ga-copy">GoAnimate © 2021</div>
-                </div>
-            </div>
-        </div>
-    </div>
-</div>
+	        PHPWrapper is a trademark of © <?php echo date("Y");?> GoAnimate Wrapper Inc |
+		<a>v1.2.3½</a>
+		<a href="https://josephcrosmanplays532.github.io/faq.html">FAQ</a>
+		<a href="https://discord.gg/yhGAetN">Wrapper Hub</a>
+	    |
+		<a href="https://josephcrosmanplays532.github.io">Server Page</a>
+		<a href="https://josephcrosmanplays532.github.io/caillou.html">Caillou</a>
+	</div>
+</footer>
 
 
 <div id="studio_container" style="display: none;">
     <div id="studio_holder"><!-- Full Screen Studio -->
         <div style="top: 50%; position: relative;">
-            You can't use Vyond Remastered because Flash might be disabled. <a href="https://drive.google.com/file/d/1g3V42Wb-N67OpVWABSyU7H3X58fwfEMI/view">Enable Flash</a>.
+            You can't use PHPWrapper because Flash might be disabled. <a href="https://josephcrosmanplays532.github.io/go/getflash">Enable Flash</a>.
         </div>
     </div>
 </div>
@@ -457,41 +419,7 @@ var folderCounts = 0;
 <script type="text/javascript">
 </script>
 
-<script type="text/javascript">
-var json;
-		var tbody = document.getElementsByTagName('tbody')[0];
-		var loadMore = document.getElementById('load_more');
-		const listReq = new XMLHttpRequest();
-		listReq.open('GET', '/movieList');
-		listReq.send();
-
-		var C = 0;
-		function loadRows() {
-			let c = C; C += 69;
-			for (; c < C; c++) {
-				if (c > json.length - 1) {
-					loadMore.remove();
-					break;
-				}
-
-				const tbl = json[c];
-				const date = tbl.date.substr(0, 10) + ' ' + tbl.date.substr(11);
-				tbody.insertAdjacentHTML('beforeend',
-					'<tr><td><img src="/movie_thumbs/' + tbl.id + '.png"></td><td><div>' + tbl.title.replace(/&/g, "&amp;").replace(/</g, "&lt;").replace(/>/g, "&gt;") + '</div><div>' + tbl.durationString + '</div></div></td><td><span>' + date + '</span></td><td><a href="javascript:;" onclick="popup(\'' + tbl.id + '\')"></a><a href="/studio?movieId=' + tbl.id + '"></a><a href="/movies/' + tbl.id + '.xml" download="' + tbl.title + '"></a></td></tr>');
-			}
-		}
-
-		loadMore.onclick = loadRows;
-		listReq.onreadystatechange = function (e) {
-			if (listReq.readyState != 4) return;
-			json = JSON.parse(listReq.responseText);
-			loadRows();
-		}
-
-		function popup(id) {
-			window.open('/videos/?movieId=' + id, 'MsgWindow', 'width=1280,height=723,left=' + (screen.width / 2 - 640) + ',top=' + (screen.height / 2 - 360));
-		}
-</script>
+<script type="text/javascript"></script>
 <style>
 .warning {
 			font-size: 14px;

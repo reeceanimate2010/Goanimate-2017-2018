fba8bdf506525e1137b25f333310cd2ca09492e8
Joseph The Animator <73652344+josephcrosmanplays532@users.noreply.github.com>
2021-07-09 15:15:09 -0400

    Create index.php

---
+++ videomaker/full/zimmertwins/index.php
@@ -0,0 +1,165 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en-local" lang="en-local" id="">
+  <head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <title>Video Studio | PHPWrapper</title>
+    <meta name="keywords" content="Animation, Games, Kids, Children, Storytelling, Stories, Movies, Movie-Maker, Learning, Literacy, Educational, Free, Activities, Elementary, Primary, Eva, Edgar, Psychic, Creative, Parents, Family, zinc Roe"/>
+    <meta name="description" content="The Zimmer Twins website invites kids to create and share their own animated stories."/>
+    <meta name="copyright" content="©<?php echo date("Y");?> PHPWrapper Zimmer Twins Theme"/>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+<link rel="shortcut icon" href="/themes/zimmertwins/images/favicon.ico" type="image/x-icon"/>
+    <link type="text/css" rel="stylesheet" media="all" href="/themes/zimmertwins/css/A.all,,_modules,,_contrib,,_cck,,_theme,,_content-module.css,,qu+all,,_modules,,_contrib,,_date,,_date.css,,qu+all,,_modules,,_contrib,,_og,,_theme,,_og.css,,qu+all,,_modules,,_contrib,,_pollfield,,_pollfield.css,,qu+all,,_modules,,_contrib,,_ubercart,,_uc_order,,_uc_order.css,,qu+all,,_modules,,_contrib,,_ubercart,,_uc_product,,_uc_product.css,,qu+all,,_modules,,_contrib,,_ubercart,,_uc_roles,,_uc_roles.css,,qu+all,,_modules,,_contrib,,_ubercart,,_uc_store,,_uc_store.css,,qu+all,,_modules,,_contrib,,_cck,,_modules,,_fieldgroup,,_fieldgroup.css,,qu+all,,_modules,,_contrib,,_views,,_css,,_views.css,,qu+zimmertwins.com,,_themes,,_zimmertwins,,_style.css,,qu,Mcc.ZqA5kNg5Py.css.pagespeed.cf.NDzs0fZpXb.css"/>
+
+
+
+
+
+
+
+
+
+
+    <!--[if IE]>
+      <link type="text/css" rel="stylesheet" media="all" href="/themes/zimmertwins/css/ie.css" />
+    <![endif]-->
+    <script src="/themes/zimmertwins/js/misc,_jquery.js,qu+misc,_drupal.js,qu+sites,_all,_modules,_contrib,_fivestar,_js,_fivestar.js,qu+sites,_all,_modules,_contrib,_google_analytics,_googleanalytics.js,qu+sites,_all,_modules,_contrib,_og,_og.js,qu+sites,_all,_modules,_contrib,_ubercart,_uc_roles,_uc_roles.js,qu+sites,_zimmertwins.com,_themes,_zimmertwins,_jquery.flash.js,qu+sites,_zimmertwins.com,_themes,_zimmertwins,_jquery.livequery.js,qu+sites,_zimmertwins.com,_themes,_zimmertwins,_scripts.js,qu.pagespeed.jc.ISNOxQFg5D.js"></script><script>eval(mod_pagespeed_lY3PXsDeiT);</script>
+<script>eval(mod_pagespeed_Aq8kdrmCeT);</script>
+<script>eval(mod_pagespeed_m_VrBtb9AX);</script>
+<script>eval(mod_pagespeed_8mSAI53C15);</script>
+<script>eval(mod_pagespeed_HYIE38osD2);</script>
+<script>eval(mod_pagespeed_yKhvl9r7EL);</script>
+<script>eval(mod_pagespeed_cUiJjLrepK);</script>
+<script>eval(mod_pagespeed_w9$j8Xny$J);</script>
+<script>eval(mod_pagespeed_1EKkpeHo72);</script>
+<script type="text/javascript">//<![CDATA[
+jQuery.extend(Drupal.settings,{"basePath":"/","fivestar":{"titleUser":"Your rating: ","titleAverage":"Average: ","feedbackSavingVote":"Saving vote...\r\n","feedbackVoteSaved":"Rating saved.","feedbackDeletingVote":"Removing vote...","feedbackVoteDeleted":"Rating removed."},"googleanalytics":{"trackMailto":1,"trackDownload":1,"trackDownloadExtensions":"7z|aac|arc|arj|asf|asx|avi|bin|csv|doc|exe|flv|gif|gz|gzip|hqx|jar|jpe?g|js|mp(2|3|4|e?g)|mov(ie)?|msi|msp|pdf|phps|png|ppt|qtm?|ra(m|r)?|sea|sit|tar|tgz|torrent|txt|wav|wma|wmv|wpd|xls|xml|z|zip"}});
+//]]></script>
+  </head>
+  <body>
+      
+    <div id="wrapper" class="clear-block">
+      
+      <div id="main">
+        
+        <div id="content-header">
+                  </div>
+        
+        <div id="content" class="clear-block">
+        
+          <h1 class="page-title">
+            <span class="page-icon"></span>
+            Create Movie          </h1>
+        
+                  
+                  
+          <div id="movie-clip-editor-0" class="movie-clip-editor flash">
+  
+  <input type="hidden" name="height" value="565"/>
+  <input type="hidden" name="src" value="https://phpwrapper.herokuapp.com/themes/zimmertwins/movie/editor.swf"/>
+  <input type="hidden" name="width" value="760"/>
+  
+  <code class="flashvars">
+  
+    <input type="hidden" name="apiurl" value="https://phpwrapper.herokuapp.com/themes/zimmertwins/movie/assets/api.xml"/>
+    <input type="hidden" name="asseturl" value="https://phpwrapper.herokuapp.com/themes/zimmertwins/movie/assets"/>
+    <input type="hidden" name="baseurl" value="https://phpwrapper.herokuapp.com"/>
+    <input type="hidden" name="lang" value="en-local"/>
+    <input type="hidden" name="userid" value=""/>
+    <input type="hidden" name="movieid" value=""/>
+    
+          <input type="hidden" name="starterid" value="590506"/>
+      <input type="hidden" name="startername" value="idolhands"/>
+        
+    <input type="hidden" name="playhelpmovie" value=""/>
+    
+  </code>
+
+</div>          
+        </div>
+        
+        <div id="content-footer">
+                  </div>
+        
+      </div>
+      
+      <div id="sidebar">
+        
+        <a id="site-logo" href="/">
+                  </a>
+        
+                <ul id="nav"><li class="menu-343 first"><a href="/" id="nav-home">Home</a></li>
+<li class="menu-345"><a href="/themes/zimmertwins/movie/watch" id="nav-watch">Watch A Movie</a></li>
+<li class="menu-344"><a href="/themes/zimmertwins/movie/starters" id="nav-make">Make A Movie</a></li>
+<li class="menu-920"><a href="/themes/zimmertwins/spotlight" id="nav-spotlight">Spotlight</a></li>
+<li class="menu-347"><a href="/themes/zimmertwins/extras" id="nav-extras">Extras</a></li>
+<li class="menu-79 last"><a href="/themes/zimmertwins/help" id="nav-help">Help</a></li>
+</ul>                
+        <div class="block block-user block-user-1">
+		  <h2>
+	    JOSHGUNNER	    	  </h2>
+		<ul class="menu"><li class="leaf first"><a href="/themes/zimmertwins/profile/view" title="The current user&#39;s profile page.">My profile</a></li>
+<li class="leaf"><a href="/videos/browse.php">My movies</a></li>
+<li class="leaf"><a href="/themes/zimmertwins/favourites" title="">My favourites</a></li>
+<li class="leaf"><a href="/themes/zimmertwins/account/416896/edit" title="">Edit account</a></li>
+<li class="leaf last"><a href="/themes/zimmertwins/logout">Log out</a></li>
+</ul></div>
+<div class="block block-movie-search block-movie-search-0">
+		  <h2>search</h2>
+		<form action="https://phpwrapper.herokuapp.com/" accept-charset="UTF-8" method="post" id="movie-clip-block-search-movie-form">
+<div><div class="form-item" id="edit-keywords-wrapper">
+ <input type="text" maxlength="50" name="keywords" id="edit-keywords" size="10" value="" class="form-text required">
+</div>
+<input type="submit" name="op" id="edit-search" value="Go" class="form-submit">
+<input type="hidden" name="form_build_id" id="form-drdnZ4HH5_Q4JleSPTxi0Lp90aP4rTKnOP9qVoZWQGc" value="form-drdnZ4HH5_Q4JleSPTxi0Lp90aP4rTKnOP9qVoZWQGc">
+<input type="hidden" name="form_id" id="edit-movie-clip-block-search-movie-form" value="movie_clip_block_search_movie_form">
+
+</div></form>
+</div>
+                
+      </div>
+      
+      <div id="sidebar-bg"></div>
+      
+      <div id="footer">
+        <span class="copyright">©<?php echo date("Y");?> PHPWrapper Zimmer Twins Theme</span>
+                  <ul class="links"><li class="menu-924 first"><a href="/themes/zimmertwins/membership" title="Get A Membership">Membership</a></li>
+<li class="menu-82"><a href="/themes/zimmertwins/about/terms" title="">Terms Of Use</a></li>
+<li class="menu-81"><a href="/themes/zimmertwins/about/privacy" title="">Privacy Policy</a></li>
+<li class="menu-83"><a href="/themes/zimmertwins/about/conduct" title="">Code Of Conduct</a></li>
+<li class="menu-87"><a href="/themes/zimmertwins/about/credits" title="">Credits</a></li>
+<li class="menu-84"><a href="/themes/zimmertwins/about/parents" title="">Parents</a></li>
+<li class="menu-348"><a href="/themes/zimmertwins/contact" title="">Contact</a></li>
+<li class="menu-1343 last"><a href="/themes/zimmertwins/about" title="About The Zimmer Twins">About</a></li>
+</ul>              </div>
+      
+    </div>
+    
+    <script type="text/javascript">//<![CDATA[
+var _gaq=_gaq||[];_gaq.push(["_setAccount","UA-295035-15"]);_gaq.push(['_setCustomVar',1,"User roles","anonymous user",1]);_gaq.push(["_trackPageview"]);(function(){var ga=document.createElement("script");ga.type="text/javascript";ga.async=true;ga.src=("https:"==document.location.protocol?"https://ssl":"http://www")+".google-analytics.com/ga.js";var s=document.getElementsByTagName("script")[0];s.parentNode.insertBefore(ga,s);})();
+//]]></script>
+    
+  </body>
+</html>
+<!--
+     FILE ARCHIVED ON 09:40:23 Jul 06, 2020 AND RETRIEVED FROM THE
+     INTERNET ARCHIVE ON 18:56:50 Jul 09, 2021.
+     JAVASCRIPT APPENDED BY WAYBACK MACHINE, COPYRIGHT INTERNET ARCHIVE.
+
+     ALL OTHER CONTENT MAY ALSO BE PROTECTED BY COPYRIGHT (17 U.S.C.
+     SECTION 108(a)(3)).
+-->
+<!--
+playback timings (ms):
+  captures_list: 1595.656
+  exclusion.robots: 1509.286
+  exclusion.robots.policy: 1509.278
+  xauthn.identify: 1406.586
+  xauthn.chkprivs: 102.462
+  RedisCDXSource: 0.732
+  esindex: 0.007
+  LoadShardBlock: 64.757 (3)
+  PetaboxLoader3.datanode: 96.27 (4)
+  CDXLines.iter: 17.517 (3)
+  load_resource: 114.752
+  PetaboxLoader3.resolve: 34.818
+-->

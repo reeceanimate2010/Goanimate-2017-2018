c76a4df1184a5709302c579b26317b0487b46918
Joseph The Animator <73652344+josephcrosmanplays532@users.noreply.github.com>
2021-07-11 14:18:29 -0400

    Update index.php

--- goapi/getTheme/index.php
+++ goapi/getTheme/index.php
@@ -6,5 +6,24 @@
 	///
 	/// Variables
 	///
-        $themeXMLZip->open("../../_themes/{$_POST["themeId"]}.zip", ZipArchive::CREATE);
+	$themeXMLZip = new ZipArchive;
+	$createZip = $themeXMLZip->open("../../_user-files/cache/theme.zip", ZipArchive::CREATE);
+
+	///
+	/// Zip creation
+	///
+	if ($createZip == true) {
+		// Add theme XML to zip file
+		$themeXMLZip->addFile("../asset/themes/{$_POST["themeId"]}/theme.xml", "theme.xml");
+		// Close zip
+		$themeXMLZip->close();
+		// Read zip
+		readfile("../../_user-files/cache/theme.zip");
+		// Delete zip for future theme loading
+		unlink("../../_user-files/cache/theme.zip");
+	}
+	// If, for some reason creating the zip fails, send the user a message explaining what went wrong.
+	else {
+		echo "ERROR: Could not create theme XML zip.";
+	}
 ?>

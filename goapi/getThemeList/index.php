3296c2d2b9f06eb0877ddc2c4a0ad5eacb2f5bd2
Joseph The Animator <73652344+josephcrosmanplays532@users.noreply.github.com>
2021-08-03 14:44:27 -0400

    Update index.php

--- goapi/getThemeList/index.php
+++ goapi/getThemeList/index.php
@@ -7,6 +7,23 @@
 	/// Variables
 	///
 	$themeListZip = new ZipArchive;
-	$themeListZip->open("../../_user-files/cache/themelist.zip");
+	$createZip = $themeListZip->open("../../_user-files/cache/themeList.zip", ZipArchive::CREATE);
+	
+	///
+	/// Zip creation
+	///
+	if ($createZip == true) {
+		// Add themelist to zip file
+		$themeListZip->addFile("../asset/themes/themelist.xml", "themelist.xml");
+		// Close zip
+		$themeListZip->close();
+		// Read zip to LVM
+		readfile("../../_user-files/cache/themeList.zip");
+		// Delete zip for future themelist loading
+		unlink("../../_user-files/cache/themeList.zip");
+	}
+	// If, for some reason creating the zip fails, send the user a message explaining what went wrong.
+	else {
+		echo "ERROR: Could not create themelist zip. Please get support here: https://discord.gg/2SUpuZnxRq";
 	}
 ?>

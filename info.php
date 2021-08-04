4abe1ec73165d5e339fb72f19c72ee92ba9fb847
Joseph The Animator <73652344+josephcrosmanplays532@users.noreply.github.com>
2021-08-04 14:54:31 -0400

    Update info.php

--- info.php
+++ info.php
@@ -1,30 +1,17 @@
 <?php echo {
 	"GET": {
-		"/index.html": {
+		"/go/character_creator": {
 			"headers": {
-				"Location": "/yourvideos"
+				"Location": "/characters/create.php"
 			},
 			"statusCode": 302,
 			"content": ""
 		},
-		"/dashboard/videos": {
+		"/videomaker/full": {
 			"headers": {
-				"Location": "/yourvideos"
+				"Location": "/videos/create.php"
 			},
 			"statusCode": 302,
 			"content": ""
-		},
-		"/$": {
-			"headers": {
-				"Location": "/yourvideos"
-			},
-			"statusCode": 302,
-			"content": ""
-		},
-		"/crossdomain.xml": {
-			"headers": {
-				"Content-Type": "text/html; charset=UTF-8"
-			},
-			"content": "<cross-domain-policy><allow-access-from domain=\"*\"/></cross-domain-policy>"
-		},
+		}
     ?>

c6d057da89efdc4e17f3e0765408731d8b18399a
Joseph The Animator <73652344+josephcrosmanplays532@users.noreply.github.com>
2021-08-04 16:19:51 -0400

    Update info.php

--- info.php
+++ info.php
@@ -1,6 +1,13 @@
 <?php
-if (!isset("/go/character_creator") {
-header("Location: /characters/create.php");
+$theme = $_POST["themeId"];
+$type = $_POST["bs"]
+$tray = $_POST["tray"]
+if (!isset("/go/character_creator/{$theme}/new_char?type={$type}") {
+header("Location: /characters/create.php?theme={$theme}&type={$type}");
 exit();
+if (!isset("/videomaker/full/{$tray}/tutorial?") {
+header("Location: /videos/create.php?tray={$tray}&interactiveTutorial=Display");
+exit();
+
 }
 ?>'

c80c14aeafd665683161d7d132b5a8bafc9fb03a
Joseph The Animator <73652344+josephcrosmanplays532@users.noreply.github.com>
2021-08-03 21:19:12 -0400

    Create index.php

---
+++ videomaker/full/index.php
@@ -0,0 +1,8 @@
+<?php
+	////
+	//// This PHP redirects you to the videomaker with the interactive tutorial page after the Studio takes you here.
+	////
+
+	// Matching theme fix will come out sometime soon
+	header("Location: /videos/create.php?interactiveTutorial=Display");
+?>

a3c5a5f0b8edcf1126893ae4e1b1c07d9e3b1782
Joseph The Animator <73652344+josephcrosmanplays532@users.noreply.github.com>
2021-08-03 22:55:19 -0400

    Update info.php

--- info.php
+++ info.php
@@ -7,18 +7,61 @@
 	/// Read file
 	///
 	echo "GET": {
-		"/go/character_creator/": {
+		"/go/character_creator/family/new_char?type=adam": {
 			"headers": {
-				"Location": "/characters/create.php"
+				"Location": "/characters/create.php?theme=family&type=adam"
 			},
 			"statusCode": 302,
 			"content": ""
 		},
-		"/videomaker/full/": {
+		"/go/character_creator/family/new_char?type=eve": {
 			"headers": {
-				"Location": "/videos/create.php?interactiveTutorial=Display"
+				"Location": "/characters/create.php?theme=family&type=eve"
 			},
 			"statusCode": 302,
 			"content": ""
 		},
+		"/go/character_creator/family/new_char?type=rocky": {
+			"headers": {
+				"Location": "/characters/create.php?theme=family&type=rocky"
+			},
+			"statusCode": 302,
+			"content": ""
+		},
+		"/go/character_creator/family/new_char?type=bob": {
+			"headers": {
+				"Location": "/characters/create.php?theme=family&type=bob"
+			},
+			"statusCode": 302,
+			"content": ""
+		},
+		"/go/character_creator/action/new_char?type=default": {
+			"headers": {
+				"Location": "/characters/create.php?theme=family&type=default"
+			},
+			"statusCode": 302,
+			"content": ""
+		},
+		"/videomaker/full/custom/tutorial": {
+			"headers": {
+				"Location": "/videos/create.php?tray=custom&interactiveTutorial=Display"
+			},
+			"statusCode": 302,
+			"content": ""
+		},
+		"/videomaker/full/action/tutorial": {
+			"headers": {
+				"Location": "/videos/create.php?tray=action&interactiveTutorial=Display"
+			},
+			"statusCode": 302,
+			"content": ""
+		},
+		"/videomaker/full/retro/tutorial": {
+			"headers": {
+				"Location": "/videos/create.php?tray=retro&interactiveTutorial=Display"
+			},
+			"statusCode": 302,
+			"content": ""
+		},
+				
     ?>

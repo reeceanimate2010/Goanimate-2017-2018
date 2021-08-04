f973a37fed8ccc5a29b294d9f64a8c6244028ddf
Joseph The Animator <73652344+josephcrosmanplays532@users.noreply.github.com>
2021-08-03 21:17:08 -0400

    Update index.php

--- go/character_creator/index.php
+++ go/character_creator/index.php
@@ -1,1 +1,9 @@
+<?php
+	////
+	//// This PHP redirects you to the character creator page after the CC Browser takes you here.
+	////
+
+	// Matching theme fix will come out sometime soon
+	header("Location: /characters/create.php");
+?>
 

d4626b85c6276db1135d2820ce230a01cbe9ce24
Joseph The Animator <73652344+josephcrosmanplays532@users.noreply.github.com>
2021-08-03 21:23:49 -0400

    Delete videomaker/full directory

--- videomaker/full/index.php
+++
@@ -1,8 +0,0 @@
-<?php
-	////
-	//// This PHP redirects you to the videomaker with the interactive tutorial page after the Studio takes you here.
-	////
-
-	// Matching theme fix will come out sometime soon
-	header("Location: /videos/create.php?interactiveTutorial=Display");
-?>

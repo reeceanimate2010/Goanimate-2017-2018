31c93bce5b8cfa1b75bb9ce1daf5b03704d52d39
Joseph The Animator <73652344+josephcrosmanplays532@users.noreply.github.com>
2021-09-03 08:20:02 -0400

    Update index.php

--- goapi/getThemeList/index.php
+++ goapi/getThemeList/index.php
@@ -1,23 +1,29 @@
 <?php
 	////
-	//// This PHP file read the themelist to the LVM
+	//// This PHP file zips the themelist and reads it to the LVM
 	////
 
 	///
 	/// Variables
 	///
 	$themeListZip = new ZipArchive;
-	$createZip = $themeListZip->open("../../_user-files/cache/themeList.zip", ZipArchive::CREATE);
+	$createZip = $themeListZip->open("../../_user-files/cache/themelist.zip", ZipArchive::CREATE);
 	
 	///
 	/// Zip creation
 	///
 	if ($createZip == true) {
-		// Read themelist xml
-		$themeListZip->readFile("../asset/themes/themelist.xml", "themelist.xml");
+		// Add themelist to zip file
+		$themeListZip->addFile("../asset/themes/themelist.xml", "themelist.xml");
+		// Close zip
+		$themeListZip->close();
+		// Read zip to LVM
+		readfile("../../_user-files/cache/themelist.zip");
+		// Delete zip for future themelist loading
+		unlink("../../_user-files/cache/themelist.zip");
 	}
 	// If, for some reason creating the zip fails, send the user a message explaining what went wrong.
 	else {
-		echo "ERROR: Could not create themelist zip. Please get support here: https://discord.io/goanimate4schools";
+		echo "ERROR: Could not create themelist zip. Please get support here: https://discord.gg/2SUpuZnxRq";
 	}
 ?>

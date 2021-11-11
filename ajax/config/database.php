8054011104e2218a54474e9259c8b1c6ce0e84db
ReeceAnimate2010 <81534627+reeceanimate2010@users.noreply.github.com>
2021-11-11 11:25:21 -0400

    Add files via upload

---
+++ ajax/config/database.php
@@ -0,0 +1,9 @@
+<?php
+	$servername = "localhost";
+        $port = "6596";
+	$username = "root";
+	$password = "";
+	$db="school";
+	/*Create connection*/
+	$conn = mysqli_connect($servername, $port, $username, $password,$db);
+?>
